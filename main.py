29ed528834cf0482616ef05c8a6f5e8a1989ebb8
JpSantos <joaomsantos2007@hotmail.com>
2026-06-24 17:23:50 -0300

    adicionei classe enemy com movimento sem sprites

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
-
+from src.entities.enemy import Enemy
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Super Mario Carne")
     clock = pygame.time.Clock()
+
+    inimigo= Enemy(400,300)
 
     running = True
     while running:
@@ -13,8 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        inimigo.update()          
 
         screen.fill((0, 0, 0))
+        inimigo.draw(screen) 
         pygame.display.flip()
 
     pygame.quit()
--- src/entities/enemy.py
+++ src/entities/enemy.py
@@ -1,2 +1,25 @@
 import pygame
+class Enemy:
+    def __init__(self, x ,y):
+        self.x=x
+        self.y=y
+        self.width=40
+        self.height=40
+        self.speed=2
+        self.image= pygame.image.load("Projeto-IP/src/images/inimigo.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.image_original = self.image
+
+    def update(self):
+        self.x += self.speed
+        if self.x > 800 - self.width:
+            self.speed = -2
+            self.image = pygame.transform.flip(self.image_original, True, False)
+        if self.x < 0:
+            self.speed = 2
+            self.image = self.image_original
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+        
 
